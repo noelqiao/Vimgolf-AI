9b826ac27b7c30756f0ca9c1194bc3132a0856fd
VoidArch <VoidArch@users.noreply.github.com>
2019-11-18 18:13:06 -0500

    Tracking modes(n/i/v/c) based on keyboard input

--- modetrack.py
+++ modetrack.py
@@ -6,35 +6,43 @@
 # insert mode = 1
 # visual mode = 2
 # command mode = 3
+
 def fun(keystrokes): 
-    # requires input to be in the form of a list like ['i', ':', '<Esc>']
-    v2i = ['a', 'A', 'i', 'I', 'o', 'O', 's', 'S']
-    v2c = [':']
-    i2v = ['`esc']
-    c2v = ['`ent', '`esc']
+    # There are very likely to be more potential situations.
+    c2n = ['`ent', '`esc']
+    i2n = ['`esc']
+    n2c = [':']
+    n2i = ['a', 'A', 'i', 'I', 'o', 'O', 'r', 'R', 's', 'S']
+    n2v = ['v', 'V']
+    v2n = ['`ent', '`esc']
     idx = 0
-    currentMode = 'v'
+    currentMode = 0
     modeList = []
     cCount = 0
     while keystrokes[idx:]:
-        if currentMode == 'v':
-            if keystrokes[idx] in v2i:
-                currentMode = 'i'
-            elif keystrokes[idx] in v2c:
-                currentMode = 'c'
-        elif currentMode == 'i':
-            if keystrokes[idx] in i2v:
-                currentMode = 'v'
+        if currentMode == 0:
+            if keystrokes[idx] in n2i:
+                currentMode = 1
+            elif keystrokes[idx] in n2c:
+                currentMode = 3
+            elif keystrokes[idx] in n2v:
+                currentMode = 2
+        elif currentMode == 1:
+            if keystrokes[idx] in i2n:
+                currentMode = 0
+        elif currentMode == 2:
+            if keystrokes[idx] in v2n:
+                currentMode = 0
         else:
-            if keystrokes[idx] in c2v:
-                currentMode = 'v';
-        if currentMode == 'c':
+            if keystrokes[idx] in c2n:
+                currentMode = 0
+        if currentMode == 3:
             if keystrokes[idx] == '`bac':
                 cCount = cCount - 1
             else:
                 cCount = cCount + 1
-            if cCount == 0: # C2V when current column is empty
-                currentMode = 'v'
+            if cCount == -1: # c2n when current column is empty
+                currentMode = 0
         modeList.append(currentMode)
         idx = idx + 1;
     return modeList
